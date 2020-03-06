1c22387be89559d72ba804ddf7e49f6fc403220c
Danielle Bergbower <dkw5011@gmail.com>
2020-03-06 02:21:19 +0000

    cli list products

--- lib/top_mom_products/cli.rb
+++ lib/top_mom_products/cli.rb
@@ -1,5 +1,19 @@
 class TopMomProduct::CLI
   def call 
     puts "Welcome to 2019 top mommy products!!!"
+    get_products
+    list_products
   end
+  
+  def get_products
+    @products = ["highchair", "stroller", "baby carrier", "baby swing"]
+end
+
+def list_products
+  @products.each.with_index(1) do |product, index|
+    puts "#{index}. #{product}"
+  end
+end
+
+
 end
