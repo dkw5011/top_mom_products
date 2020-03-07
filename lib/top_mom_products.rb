e3396796f0c5fa426b6e57629b2e88c9e8642bc6
Danielle Bergbower <dkw5011@gmail.com>
2020-03-07 03:32:53 +0000

    create scraper file

--- lib/top_mom_products.rb
+++ lib/top_mom_products.rb
@@ -1,5 +1,7 @@
 require_relative "top_mom_products/version"
 require_relative "top_mom_products/cli"
+require_relative "top_mom_products/product"
+require_relative "top_mom_products/scraper"
 
 module TopMomProducts
   class Error < StandardError; end
