b1de069a9f4bfe7328e79f5fc9c5db879178e278
Danielle Bergbower <dkw5011@gmail.com>
2020-03-08 13:03:44 +0000

    new site add

--- lib/top_mom_products/scraper.rb
+++ lib/top_mom_products/scraper.rb
@@ -1,8 +1,8 @@
 class TopMomProducts::Scraper
   def self.product_scraper
-    page = Nokogiri::HTML(open("https://www.workingmother.com/best-new-baby-products-working-moms"))
+    page = Nokogiri::HTML(open("https://www.babylist.com/hello-baby/best-baby-products"))
     
-    products = page.css("in-content-skip")
+    products = page.css(".product-title").text
     
     binding.pry
 end
